72c3d3305a6345a0edf0d897bcd5a137871f9f8f
Thao Nguyen <nptjr29@yahoo.com>
2017-02-15 09:17:56 -0800

    Fixed is_in_array() to work with 2D char array

--- Assignment3_Q1.cpp
+++ Assignment3_Q1.cpp
@@ -2,11 +2,8 @@
 #include <string>
 using namespace std;
 
-// Checks if a string s is a reserved word
-bool is_reserved(const string& s, char arr[][10], int row, int col);
-
-// Check is a string s is a special symbol
-bool is_special(const string& s, char arr[][3], int row, int col);
+// Check if string s is in an array arr
+bool is_in_array(const string& s, char * arr, int row, int col);
 
 // Checks if a string s is a number
 bool is_number(const string& s);
@@ -43,13 +40,13 @@ int main()
 			else
 			{
 				//if token is a reserved word
-				if (is_reserved(temp, reservedWords, 4, 10))
+				if (is_in_array(temp, (char *)reservedWords, 4, 10))
 				{
 					cout.width(W); 
 					cout << left << temp << ": " << "reserved word" << endl;
 				}					
 				//if token is a special symbol
-				else if (is_special(temp, special, 8, 3))
+				else if (is_in_array(temp, (char *)special, 8, 3))
 				{
 					cout.width(W); 
 					cout << left << temp << ": " << "special symbol" << endl;
@@ -95,35 +92,16 @@ int main()
 }
 // Ends of main() program
 
-
-// Checks if a string s is a reserved word
-bool is_reserved(const string& s, char arr[][10], int row, int col)
-{ 
-	for (int i = 0; i < row; ++i)
-	{
-		int j = 0;
-		string temp = "";
-		while (arr[i][j] != '\0')
-		{
-			temp += arr[i][j];
-			++j;
-		}
-		if (s.compare(temp) == 0)
-			return true;
-	}
-	return false;
-}
-
-// Checks if a string s is a special character
-bool is_special(const string& s, char arr[][3], int row, int col)
+// Checks if a string s is in an array arr
+bool is_in_array(const string& s, char * arr, int row, int col)
 {
 	for (int i = 0; i < row; ++i)
 	{
 		int j = 0;
 		string temp = "";
-		while (arr[i][j] != '\0')
+		while (*((arr + i*col) + j) != '\0')
 		{
-			temp += arr[i][j];
+			temp += *((arr + i*col) + j);
 			++j;
 		}
 		if (s.compare(temp) == 0)
