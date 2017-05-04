3c6af68870a837e9a2ca136642f27b595921e964
Thao Nguyen <nptjr29@yahoo.com>
2017-05-04 14:11:27 -0700

    Create finalv3.cpp

---
+++ FinalProject/finalv3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int  P1 , Q2S , R , S12 ; 
+int main() {
+P1 = - 3 ; 
+Q2S = 4 ; 
+R = 5 ; 
+cout << R ;
+S12 = P1 * ( Q2S + 2 * R ) ; 
+cout << S12 ;
+return 0;
+}
